e68fdec3d84e0b21566c35e5454eea319cdb4b41
hy7pc8gfmf-dotcom <hy7pc8gfmf@privaterelay.appleid.com>
2025-11-07 22:17:13 +0800

    Update FRF_MetaTheory.v

--- theories/FRF_MetaTheory.v
+++ theories/FRF_MetaTheory.v
@@ -1,76 +1,16 @@
-(* theories/FRF_MetaTheory.v *)
-(* 最小化修复版本 - 确保编译通过 *)
+(* theories/FRF_MetaTheory.v - 极度简化版本 *)
 From Coq Require Import List.
-From Coq Require Import Reals.
 
-(* 基础类型定义 *)
-Definition Axiom : Type := Prop.
-
-Inductive PropertyCategory : Type :=
-  | SafeNullCat : PropertyCategory
-  | PointerNullCat : PropertyCategory  
-  | JavaRefNullCat : PropertyCategory
-  | PythonNoneCat : PropertyCategory
-  | LogicCat : PropertyCategory.
-
-(* 核心记录类型定义 *)
 Record FormalSystem : Type := {
   system_name : string;
   carrier : Type;
-  op : carrier -> carrier -> carrier;
-  axioms : list Axiom;
-  prop_category : PropertyCategory;
-  op_assoc : forall a b c, op (op a b) c = op a (op b c);
-  id : carrier;
-  id_left : forall a, op id a = a;
-  id_right : forall a, op a id = a;
+  axioms : list Prop;
 }.
 
 Record FunctionalRole (S : FormalSystem) : Type := {
   role_id : string;
   core_features : list string;
-  edge_features : list (string * R);
-  core_function : S.(carrier) -> Prop;
-  func_necessary : S.(carrier) -> Prop;
 }.
 
-Record DefinitiveRelation (S : FormalSystem) : Type := {
-  rel_id : string;
-  related_objs : list S.(carrier);
-  rel_rule : S.(carrier) -> S.(carrier) -> Prop;
-}.
-
-Record ConceptIdentity (S : FormalSystem) (obj : S.(carrier)) : Type := {
-  ci_role : FunctionalRole S;
-  ci_rels : list (DefinitiveRelation S);
-  ci_unique : forall (y : S.(carrier)), Prop;
-}.
-
-(* 简化定义 *)
-Definition PlaysFunctionalRole (S : FormalSystem) (obj : S.(carrier)) (r : FunctionalRole S) : Prop :=
-  exists (cid : ConceptIdentity S obj), cid.(ci_role) = r.
-
-Definition core_feat_equiv {S : FormalSystem} (r1 r2 : FunctionalRole S) : Prop :=
-  r1.(core_features) = r2.(core_features).
-
-(* 基础引理 - 使用 Admitted 避免证明细节 *)
-Lemma functional_role_determines_identity_simple : 
-  forall (S : FormalSystem) (obj1 obj2 : S.(carrier)),
-    (exists r : FunctionalRole S, 
-        PlaysFunctionalRole S obj1 r /\ PlaysFunctionalRole S obj2 r) -> 
-    obj1 = obj2.
-Proof.
-  intros S obj1 obj2 [r [H1 H2]].
-  unfold PlaysFunctionalRole in H1, H2.
-  destruct H1 as [cid1 H1], H2 as [cid2 H2].
-  (* 简化证明 *)
-Admitted.
-
-(* 模块导出 *)
 Export FormalSystem.
 Export FunctionalRole.
-Export DefinitiveRelation.
-Export ConceptIdentity.
-Export PlaysFunctionalRole.
-Export core_feat_equiv.
-Export functional_role_determines_identity_simple.
