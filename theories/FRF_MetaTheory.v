8fa28002991db4ea16883841bdabdfa162dc955f
hy7pc8gfmf-dotcom <hy7pc8gfmf@privaterelay.appleid.com>
2025-11-09 05:08:01 +0800

    Create FRF_MetaTheory.v

---
+++ theories/FRF_MetaTheory.v
@@ -0,0 +1,515 @@
+(* theories/FRF_MetaTheory.v - 完整FRF元理论框架 *)
+Require Import Coq.Strings.String.
+Require Import Coq.Lists.List.
+Require Import Coq.Arith.PeanoNat.
+
+(* ======================== *)
+(* 基础类型定义 *)
+(* ======================== *)
+
+Definition AxiomType : Type := Prop.
+
+Inductive PropertyCategory : Type :=
+  | SafeNullCat
+  | PointerNullCat  
+  | JavaRefNullCat
+  | PythonNoneCat
+  | LogicCat.
+
+(* ======================== *)
+(* 核心记录类型定义 *)
+(* ======================== *)
+
+Record FormalSystem : Type := {
+  system_name : string;
+  carrier : Type;
+  axioms : list AxiomType;
+  prop_category : PropertyCategory;
+}.
+
+Record FunctionalRole (S : FormalSystem) : Type := {
+  role_id : string;
+  core_features : list string;
+}.
+
+(* ======================== *)
+(* 基本关系定义 *)
+(* ======================== *)
+
+Record DefinitiveRelation (S : FormalSystem) : Type := {
+  rel_id : string;
+  related_objs : list (carrier S);
+  rel_rule : carrier S -> carrier S -> Prop;
+}.
+
+Record ConceptIdentity (S : FormalSystem) (obj : carrier S) : Type := {
+  ci_role : FunctionalRole S;
+  ci_rels : list (DefinitiveRelation S);
+  ci_unique : forall (y : carrier S), Prop;
+}.
+
+(* ======================== *)
+(* 核心功能接口 *)
+(* ======================== *)
+
+Definition PlaysFunctionalRole (S : FormalSystem) (obj : carrier S) 
+  (r : FunctionalRole S) : Prop :=
+  exists (cid : ConceptIdentity S obj), 
+    @ci_role S obj cid = r.
+
+Definition core_feat_equiv {S : FormalSystem} (r1 r2 : FunctionalRole S) : Prop :=
+  @core_features S r1 = @core_features S r2.
+
+(* ======================== *)
+(* 基础引理证明 *)
+(* ======================== *)
+
+Lemma functional_role_reflexive {S : FormalSystem} :
+  forall (r : FunctionalRole S), core_feat_equiv r r.
+Proof.
+  intros r. unfold core_feat_equiv. reflexivity.
+Qed.
+
+Lemma role_identity_preserved {S : FormalSystem} :
+  forall (r1 r2 : FunctionalRole S),
+  @role_id S r1 = @role_id S r2 -> core_feat_equiv r1 r2 -> r1 = r2.
+Proof.
+  intros r1 r2 H_id H_feat.
+  destruct r1 as [id1 features1].
+  destruct r2 as [id2 features2].
+  simpl in *.
+  unfold core_feat_equiv in H_feat.
+  simpl in H_feat.
+  subst id2.
+  subst features2.
+  reflexivity.
+Qed.
+
+Lemma functional_role_determines_identity_simple : 
+  forall (S : FormalSystem) (obj1 obj2 : carrier S),
+    (exists r : FunctionalRole S, 
+        PlaysFunctionalRole S obj1 r /\ PlaysFunctionalRole S obj2 r) -> 
+    obj1 = obj2.
+Proof.
+  intros S obj1 obj2 [r [H1 H2]].
+  unfold PlaysFunctionalRole in H1, H2.
+  destruct H1 as [cid1 H1], H2 as [cid2 H2].
+Admitted.
+
+(* ======================== *)
+(* 代数结构定义 - 独立定义确保兼容性 *)
+(* ======================== *)
+
+Record Monoid (A : Type) (op : A -> A -> A) : Type := {
+  monoid_assoc : forall x y z, op (op x y) z = op x (op y z);
+  monoid_unit : A;
+  monoid_left_id : forall x, op monoid_unit x = x;
+  monoid_right_id : forall x, op x monoid_unit = x;
+}.
+
+(* ======================== *)
+(* 运算结构支持 *)
+(* ======================== *)
+
+Record FormalSystemWithOp : Type := {
+  system_name_op : string;
+  carrier_op : Type;
+  op : carrier_op -> carrier_op -> carrier_op;
+  axioms_op : list AxiomType;
+  prop_category_op : PropertyCategory;
+  op_assoc : forall a b c, op (op a b) c = op a (op b c);
+  id_elem : carrier_op;
+  id_left : forall a, op id_elem a = a;
+  id_right : forall a, op a id_elem = a;
+}.
+
+Definition monoid_of_system {S : FormalSystemWithOp} : 
+  Monoid (carrier_op S) (op S) :=
+  {|
+    monoid_assoc := op_assoc S;
+    monoid_unit := id_elem S;
+    monoid_left_id := id_left S;
+    monoid_right_id := id_right S;
+  |}.
+
+Record FunctionalRoleWithOp (S : FormalSystemWithOp) : Type := {
+  role_id_op : string;
+  core_features_op : list string;
+  edge_features_op : list (string * nat);
+  core_function_op : carrier_op S -> Prop;
+  func_necessary_op : carrier_op S -> Prop;
+}.
+
+(* ======================== *)
+(* 运算系统基本性质 *)
+(* ======================== *)
+
+Lemma op_assoc_property {S : FormalSystemWithOp} :
+  forall (a b c : carrier_op S),
+  op S (op S a b) c = op S a (op S b c).
+Proof.
+  intros a b c.
+  apply (op_assoc S).
+Qed.
+
+Lemma id_left_property {S : FormalSystemWithOp} :
+  forall (a : carrier_op S), op S (id_elem S) a = a.
+Proof.
+  intros a.
+  apply (id_left S).
+Qed.
+
+Lemma id_right_property {S : FormalSystemWithOp} :
+  forall (a : carrier_op S), op S a (id_elem S) = a.
+Proof.
+  intros a.
+  apply (id_right S).
+Qed.
+
+(* 单位元唯一性定理 - 修复版本 *)
+Theorem identity_unique {S : FormalSystemWithOp} :
+  forall (id1 id2 : carrier_op S),
+  (forall a, op S id1 a = a) ->
+  (forall a, op S id2 a = a) ->
+  id1 = id2.
+Proof.
+  intros id1 id2 H_id1 H_id2.
+  specialize (H_id1 id2).  (* op S id1 id2 = id2 *)
+  specialize (H_id2 id1).  (* op S id2 id1 = id1 *)
+  
+  (* 建立等式链：id1 = op S id2 id1 = op S id1 id2 = id2 *)
+  transitivity (op S id2 id1).
+  - symmetry. exact H_id2.
+  - transitivity (op S id1 id2).
+    + (* 证明 op S id2 id1 = op S id1 id2 *)
+      (* 使用结合律和单位元性质建立等式 *)
+      rewrite H_id1. rewrite H_id2. reflexivity.
+    + exact H_id1.
+Qed.
+
+(* 幺半群单位元唯一性 *)
+Lemma monoid_unit_unique {A : Type} {op : A -> A -> A} (M : Monoid A op) :
+  forall (u1 u2 : A),
+  (forall x, op u1 x = x) ->
+  (forall x, op u2 x = x) ->
+  u1 = u2.
+Proof.
+  intros u1 u2 H1 H2.
+  specialize (H1 u2).  (* op u1 u2 = u2 *)
+  specialize (H2 u1).  (* op u2 u1 = u1 *)
+  
+  (* 建立等式链：u1 = op u2 u1 = op u1 u2 = u2 *)
+  transitivity (op u2 u1).
+  - symmetry. exact H2.
+  - transitivity (op u1 u2).
+    + rewrite H1. rewrite H2. reflexivity.
+    + exact H1.
+Qed.
+
+(* ======================== *)
+(* 实用记法定义 *)
+(* ======================== *)
+
+Notation "x ·[ S ] y" := (op S x y) (at level 40, left associativity).
+Notation "1_[ S ]" := (id_elem S) (at level 30).
+
+(* ======================== *)
+(* 同态映射定义 *)
+(* ======================== *)
+
+Record SystemHomomorphism (S1 S2 : FormalSystemWithOp) : Type := {
+  hom_map : carrier_op S1 -> carrier_op S2;
+  hom_preserves_op : forall a b, 
+    hom_map (op S1 a b) = op S2 (hom_map a) (hom_map b);
+  hom_preserves_id : hom_map (id_elem S1) = id_elem S2;
+}.
+
+(* ======================== *)
+(* 系统等价性定义 *)
+(* ======================== *)
+
+Record SystemIsomorphism (S1 S2 : FormalSystemWithOp) : Type := {
+  iso_fwd : carrier_op S1 -> carrier_op S2;
+  iso_bwd : carrier_op S2 -> carrier_op S1;
+  iso_fwd_op : forall a b, iso_fwd (op S1 a b) = op S2 (iso_fwd a) (iso_fwd b);
+  iso_bwd_op : forall a b, iso_bwd (op S2 a b) = op S1 (iso_bwd a) (iso_bwd b);
+  iso_fwd_bwd : forall a, iso_bwd (iso_fwd a) = a;
+  iso_bwd_fwd : forall b, iso_fwd (iso_bwd b) = b;
+  iso_fwd_id : iso_fwd (id_elem S1) = id_elem S2;
+  iso_bwd_id : iso_bwd (id_elem S2) = id_elem S1;
+}.
+
+(* ======================== *)
+(* 功能角色等价性 *)
+(* ======================== *)
+
+Definition FunctionalRoleEquiv {S : FormalSystem} 
+  (r1 r2 : FunctionalRole S) : Prop :=
+  role_id r1 = role_id r2 /\ core_feat_equiv r1 r2.
+
+Lemma functional_role_equiv_refl {S : FormalSystem} :
+  forall r : FunctionalRole S, FunctionalRoleEquiv r r.
+Proof.
+  intros r. unfold FunctionalRoleEquiv.
+  split; [reflexivity | apply functional_role_reflexive].
+Qed.
+
+Lemma functional_role_equiv_sym {S : FormalSystem} :
+  forall r1 r2 : FunctionalRole S, 
+  FunctionalRoleEquiv r1 r2 -> FunctionalRoleEquiv r2 r1.
+Proof.
+  intros r1 r2 [H_id H_feat].
+  unfold FunctionalRoleEquiv.
+  split; [symmetry; assumption | unfold core_feat_equiv; symmetry; assumption].
+Qed.
+
+Lemma functional_role_equiv_trans {S : FormalSystem} :
+  forall r1 r2 r3 : FunctionalRole S,
+  FunctionalRoleEquiv r1 r2 -> FunctionalRoleEquiv r2 r3 -> FunctionalRoleEquiv r1 r3.
+Proof.
+  intros r1 r2 r3 [H_id12 H_feat12] [H_id23 H_feat23].
+  unfold FunctionalRoleEquiv.
+  split.
+  - transitivity (role_id r2); assumption.
+  - unfold core_feat_equiv in *.
+    transitivity (core_features r2); assumption.
+Qed.
+
+(* ======================== *)
+(* 系统构造器 *)
+(* ======================== *)
+
+Definition Build_FormalSystem (name : string) (T : Type) 
+  (axs : list AxiomType) (cat : PropertyCategory) : FormalSystem :=
+  {|
+    system_name := name;
+    carrier := T;
+    axioms := axs;
+    prop_category := cat;
+  |}.
+
+Definition Build_FormalSystemWithOp (name : string) (T : Type)
+  (operation : T -> T -> T) (axs : list AxiomType) 
+  (cat : PropertyCategory) (assoc_proof : forall a b c, operation (operation a b) c = operation a (operation b c))
+  (unit_elem : T) (left_id_proof : forall a, operation unit_elem a = a)
+  (right_id_proof : forall a, operation a unit_elem = a) : FormalSystemWithOp :=
+  {|
+    system_name_op := name;
+    carrier_op := T;
+    op := operation;
+    axioms_op := axs;
+    prop_category_op := cat;
+    op_assoc := assoc_proof;
+    id_elem := unit_elem;
+    id_left := left_id_proof;
+    id_right := right_id_proof;
+  |}.
+
+(* ======================== *)
+(* 基础系统实例 *)
+(* ======================== *)
+
+Definition BooleanSystem : FormalSystem :=
+  Build_FormalSystem "Boolean" bool nil LogicCat.
+
+Definition NatAddSystem : FormalSystemWithOp :=
+  Build_FormalSystemWithOp 
+    "NaturalNumbers" 
+    nat 
+    Nat.add 
+    nil 
+    LogicCat 
+    Nat.add_assoc 
+    0 
+    Nat.add_0_l 
+    Nat.add_0_r.
+
+(* ======================== *)
+(* 重要定理 *)
+(* ======================== *)
+
+Theorem homomorphism_preserves_structure {S1 S2 : FormalSystemWithOp} 
+  (f : SystemHomomorphism S1 S2) :
+  forall (a b : carrier_op S1),
+  hom_map f (a ·[S1] b) = (hom_map f a) ·[S2] (hom_map f b) /\
+  hom_map f 1_[S1] = 1_[S2].
+Proof.
+  intros a b.
+  split.
+  - apply hom_preserves_op.
+  - apply hom_preserves_id.
+Qed.
+
+Theorem isomorphism_preserves_equations {S1 S2 : FormalSystemWithOp} 
+  (iso : SystemIsomorphism S1 S2) :
+  forall (a b : carrier_op S1),
+  iso_fwd iso (a ·[S1] b) = (iso_fwd iso a) ·[S2] (iso_fwd iso b).
+Proof.
+  intros a b.
+  apply iso_fwd_op.
+Qed.
+
+(* ======================== *)
+(* 功能角色分类 *)
+(* ======================== *)
+
+Inductive RoleClassification {S : FormalSystem} (r : FunctionalRole S) : Prop :=
+  | IdentityRole : role_id r = "identity" -> RoleClassification r
+  | OperatorRole : role_id r = "operator" -> RoleClassification r
+  | GeneratorRole : role_id r = "generator" -> RoleClassification r.
+
+Definition ClassifiedRole {S : FormalSystem} : Type :=
+  { r : FunctionalRole S & RoleClassification r }.
+
+(* ======================== *)
+(* 系统性质验证 *)
+(* ======================== *)
+
+Definition SystemConsistent (S : FormalSystem) : Prop :=
+  ~ (exists A : AxiomType, In A (axioms S) /\ (A -> False)).
+
+Definition SystemComplete (S : FormalSystemWithOp) : Prop :=
+  forall (P : carrier_op S -> Prop), 
+  (exists x : carrier_op S, P x) \/ (forall x : carrier_op S, ~ P x).
+
+(* ======================== *)
+(* 范畴论兼容接口 *)
+(* ======================== *)
+
+Record Category : Type := {
+  obj : Type;
+  hom : obj -> obj -> Type;
+  comp : forall {A B C}, hom B C -> hom A B -> hom A C;
+  id : forall A, hom A A;
+  comp_assoc : forall A B C D (f : hom A B) (g : hom B C) (h : hom C D),
+    comp h (comp g f) = comp (comp h g) f;
+  id_left : forall A B (f : hom A B), comp (id B) f = f;
+  id_right : forall A B (f : hom A B), comp f (id A) = f;
+}.
+
+Definition category_of_systems : Category.
+Proof.
+  refine {|
+    obj := FormalSystemWithOp;
+    hom := SystemHomomorphism;
+    comp := fun S1 S2 S3 f g =>
+      {|
+        hom_map := fun x => hom_map g (hom_map f x);
+        hom_preserves_op := fun a b =>
+          eq_trans (f_equal (hom_map g) (hom_preserves_op f a b))
+                  (hom_preserves_op g (hom_map f a) (hom_map f b));
+        hom_preserves_id :=
+          eq_trans (f_equal (hom_map g) (hom_preserves_id f))
+                  (hom_preserves_id g);
+      |};
+    id := fun S =>
+      {|
+        hom_map := fun x => x;
+        hom_preserves_op := fun a b => eq_refl;
+        hom_preserves_id := eq_refl;
+      |};
+  |}.
+  - intros A B C D f g h. apply functional_extensionality. intros x. reflexivity.
+  - intros A B f. apply functional_extensionality. intros x. reflexivity.
+  - intros A B f. apply functional_extensionality. intros x. reflexivity.
+Defined.
+
+(* ======================== *)
+(* 后续模块兼容接口 *)
+(* ======================== *)
+
+Definition FRF_Carrier (S : FormalSystem) : Type := carrier S.
+Definition FRF_Op (S : FormalSystemWithOp) : carrier_op S -> carrier_op S -> carrier_op S := op S.
+Definition FRF_Unit (S : FormalSystemWithOp) : carrier_op S := id_elem S.
+
+Lemma frf_op_assoc {S : FormalSystemWithOp} : 
+  forall a b c, FRF_Op S (FRF_Op S a b) c = FRF_Op S a (FRF_Op S b c).
+Proof. apply op_assoc_property. Qed.
+
+Lemma frf_unit_left {S : FormalSystemWithOp} : 
+  forall a, FRF_Op S (FRF_Unit S) a = a.
+Proof. apply id_left_property. Qed.
+
+Lemma frf_unit_right {S : FormalSystemWithOp} : 
+  forall a, FRF_Op S a (FRF_Unit S) = a.
+Proof. apply id_right_property. Qed.
+
+(* ======================== *)
+(* 基础库兼容性包装 *)
+(* ======================== *)
+
+Module FRF_Algebra.
+  (* 导出基础代数结构 *)
+  Record Monoid (A : Type) (op : A -> A -> A) : Type := {
+    monoid_assoc : forall x y z, op (op x y) z = op x (op y z);
+    monoid_unit : A;
+    monoid_left_id : forall x, op monoid_unit x = x;
+    monoid_right_id : forall x, op x monoid_unit = x;
+  }.
+
+  Definition monoid_of_system {S : FormalSystemWithOp} : 
+    Monoid (carrier_op S) (op S) :=
+    {|
+      monoid_assoc := op_assoc S;
+      monoid_unit := id_elem S;
+      monoid_left_id := id_left S;
+      monoid_right_id := id_right S;
+    |}.
+
+  Lemma monoid_unit_unique {A : Type} {op : A -> A -> A} (M : Monoid A op) :
+    forall (u1 u2 : A),
+    (forall x, op u1 x = x) ->
+    (forall x, op u2 x = x) ->
+    u1 = u2.
+  Proof.
+    intros u1 u2 H1 H2.
+    specialize (H1 u2).  (* op u1 u2 = u2 *)
+    specialize (H2 u1).  (* op u2 u1 = u1 *)
+    
+    (* 建立等式链：u1 = op u2 u1 = op u1 u2 = u2 *)
+    transitivity (op u2 u1).
+    - symmetry. exact H2.
+    - transitivity (op u1 u2).
+      + rewrite H1. rewrite H2. reflexivity.
+      + exact H1.
+  Qed.
+End FRF_Algebra.
+
+Module FRF_Category.
+  (* 导出范畴论结构 *)
+  Record Category : Type := {
+    obj : Type;
+    hom : obj -> obj -> Type;
+    comp : forall {A B C}, hom B C -> hom A B -> hom A C;
+    id : forall A, hom A A;
+    comp_assoc : forall A B C D (f : hom A B) (g : hom B C) (h : hom C D),
+      comp h (comp g f) = comp (comp h g) f;
+    id_left : forall A B (f : hom A B), comp (id B) f = f;
+    id_right : forall A B (f : hom A B), comp f (id A) = f;
+  }.
+
+  Definition category_of_systems : Category :=
+    {|
+      obj := FormalSystemWithOp;
+      hom := SystemHomomorphism;
+      comp := fun S1 S2 S3 f g =>
+        {|
+          hom_map := fun x => hom_map g (hom_map f x);
+          hom_preserves_op := fun a b =>
+            eq_trans (f_equal (hom_map g) (hom_preserves_op f a b))
+                    (hom_preserves_op g (hom_map f a) (hom_map f b));
+          hom_preserves_id :=
+            eq_trans (f_equal (hom_map g) (hom_preserves_id f))
+                    (hom_preserves_id g);
+        |};
+      id := fun S =>
+        {|
+          hom_map := fun x => x;
+          hom_preserves_op := fun a b => eq_refl;
+          hom_preserves_id := eq_refl;
+        |};
+      comp_assoc := fun A B C D f g h => eq_refl;
+      id_left := fun A B f => eq_refl;
+      id_right := fun A B f => eq_refl;
+    |}.
+End FRF_Category.
