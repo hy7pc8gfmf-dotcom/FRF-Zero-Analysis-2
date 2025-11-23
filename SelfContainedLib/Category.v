08a281238491f69673833eba4254adece649de3b
hy7pc8gfmf-dotcom <hy7pc8gfmf@privaterelay.appleid.com>
2025-11-24 01:01:35 +0800

    3

--- SelfContainedLib/Category.v
+++ SelfContainedLib/Category.v
@@ -4,254 +4,201 @@ From Coq Require Import Utf8.
 From Coq Require Import FunctionalExtensionality.
 
 (** 范畴论基础模块 *)
+
 Module Type BasicCategory.
   Parameter Obj : Type.
   Parameter Hom : Obj -> Obj -> Type.
   Parameter id : forall A, Hom A A.
   Parameter comp : forall A B C, Hom B C -> Hom A B -> Hom A C.
   
-  (* 左单位律：id_B ∘ f = f *)
-  Axiom id_left : forall A B (f : Hom A B), comp A B B (id B) f = f.
+  (* 范畴公理 *)
+  Axiom comp_assoc : forall A B C D (f : Hom A B) (g : Hom B C) (h : Hom C D),
+      comp A C D h (comp A B C g f) = comp A B D (comp B C D h g) f.
   
-  (* 右单位律：f ∘ id_A = f *)
-  Axiom id_right : forall A B (f : Hom A B), comp A A B f (id A) = f.
+  Axiom id_left : forall A B (f : Hom A B),
+      comp A A B f (id A) = f.
   
-  (* 结合律 *)
-  Axiom assoc : forall A B C D (f : Hom A B) (g : Hom B C) (h : Hom C D),
-    comp A C D h (comp A B C g f) = comp A B D (comp B C D h g) f.
+  Axiom id_right : forall A B (f : Hom A B),
+      comp A B B (id B) f = f.
 End BasicCategory.
 
 (** 集合范畴实现 *)
 Module SetCategory <: BasicCategory.
   Definition Obj := Type.
   Definition Hom (A B : Type) := A -> B.
-  Definition id A (x : A) := x.
-  Definition comp A B C (g : B -> C) (f : A -> B) := fun x => g (f x).
-
-  Lemma id_left A B (f : A -> B) : comp A B B (id B) f = f.
-  Proof. 
-    apply functional_extensionality; 
-    intro x; 
-    reflexivity. 
-  Qed.
-
-  Lemma id_right A B (f : A -> B) : comp A A B f (id A) = f.
+  
+  Definition id (A : Type) : Hom A A := fun x => x.
+  
+  Definition comp (A B C : Type) (g : Hom B C) (f : Hom A B) : Hom A C :=
+    fun x => g (f x).
+  
+  Lemma comp_assoc : forall A B C D (f : Hom A B) (g : Hom B C) (h : Hom C D),
+      comp A C D h (comp A B C g f) = comp A B D (comp B C D h g) f.
   Proof.
-    apply functional_extensionality;
-    intro x;
+    intros A B C D f g h.
+    apply functional_extensionality.
+    intro x.
+    unfold comp.
     reflexivity.
   Qed.
-
-  Lemma assoc A B C D (f : A -> B) (g : B -> C) (h : C -> D) :
-    comp A C D h (comp A B C g f) = comp A B D (comp B C D h g) f.
+  
+  Lemma id_left : forall A B (f : Hom A B),
+      comp A A B f (id A) = f.
   Proof.
-    apply functional_extensionality;
-    intro x;
+    intros A B f.
+    apply functional_extensionality.
+    intro x.
+    unfold comp, id.
+    reflexivity.
+  Qed.
+  
+  Lemma id_right : forall A B (f : Hom A B),
+      comp A B B (id B) f = f.
+  Proof.
+    intros A B f.
+    apply functional_extensionality.
+    intro x.
+    unfold comp, id.
     reflexivity.
   Qed.
 End SetCategory.
 
-(** 离散范畴实现 *)
+(** 离散范畴实现 - 每个对象只有恒等态射 *)
 Module DiscreteCategory (T : Type) <: BasicCategory.
   Definition Obj := T.
-  Definition Hom (A B : T) := A = B.
   
-  Definition id A : Hom A A := eq_refl.
+  Inductive Hom (A B : T) : Type :=
+  | identity : A = B -> Hom A B.
   
-  Definition comp A B C (g : Hom B C) (f : Hom A B) :=
+  Definition id (A : T) : Hom A A := identity A A (eq_refl A).
+  
+  Definition comp (A B C : T) (g : Hom B C) (f : Hom A B) : Hom A C :=
     match f, g with
-    | eq_refl, eq_refl => eq_refl
+    | identity _ _ pf, identity _ _ pg =>
+        identity A C (eq_trans pf pg)
     end.
-
-  Lemma id_left A B (f : Hom A B) : comp A B B (id B) f = f.
+  
+  Lemma comp_assoc : forall A B C D (f : Hom A B) (g : Hom B C) (h : Hom C D),
+      comp A C D h (comp A B C g f) = comp A B D (comp B C D h g) f.
   Proof.
-    intros.
-    destruct f.
-    reflexivity.
+    intros A B C D f g h.
+    destruct f as [pf].
+    destruct g as [pg].
+    destruct h as [ph].
+    unfold comp.
+    f_equal.
+    apply eq_trans_assoc.
   Qed.
-
-  Lemma id_right A B (f : Hom A B) : comp A A B f (id A) = f.
+  
+  Lemma id_left : forall A B (f : Hom A B),
+      comp A A B f (id A) = f.
   Proof.
-    intros.
-    destruct f.
-    reflexivity.
+    intros A B f.
+    destruct f as [pf].
+    unfold comp, id.
+    f_equal.
+    apply eq_trans_refl_l.
   Qed.
-
-  Lemma assoc A B C D (f : Hom A B) (g : Hom B C) (h : Hom C D) :
-    comp A C D h (comp A B C g f) = comp A B D (comp B C D h g) f.
+  
+  Lemma id_right : forall A B (f : Hom A B),
+      comp A B B (id B) f = f.
   Proof.
-    intros.
-    destruct f, g, h.
-    reflexivity.
+    intros A B f.
+    destruct f as [pf].
+    unfold comp, id.
+    f_equal.
+    apply eq_trans_refl_r.
   Qed.
 End DiscreteCategory.
 
-(** 对偶范畴 *)
-Module OppositeCategory (C : BasicCategory) <: BasicCategory.
-  Definition Obj := C.Obj.
-  
-  Definition Hom (A B : C.Obj) := C.Hom B A.
-  
-  Definition id A : Hom A A := C.id A.
-  
-  Definition comp A B C (g : Hom B C) (f : Hom A B) := 
-    C.comp B A C f g.  (* 注意：这里顺序交换了 *)
-
-  Lemma id_left A B (f : Hom A B) : comp A B B (id B) f = f.
-  Proof.
-    apply C.id_right.
-  Qed.
-
-  Lemma id_right A B (f : Hom A B) : comp A A B f (id A) = f.
-  Proof.
-    apply C.id_left.
-  Qed.
-
-  Lemma assoc A B C D (f : Hom A B) (g : Hom B C) (h : Hom C D) :
-    comp A C D h (comp A B C g f) = comp A B D (comp B C D h g) f.
-  Proof.
-    (* 注意：在opposite范畴中结合律的方向会改变 *)
-    intros.
-    simpl.
-    rewrite C.assoc.
-    reflexivity.
-  Qed.
-End OppositeCategory.
-
 (** 函子定义 *)
-Module Type BasicFunctor.
-  Parameter Dom : BasicCategory.
-  Parameter Cod : BasicCategory.
+Module Type Functor (C1 C2 : BasicCategory).
+  Parameter F_Obj : C1.(Obj) -> C2.(Obj).
+  Parameter F_Hom : forall A B, C1.(Hom) A B -> C2.(Hom) (F_Obj A) (F_Obj B).
   
-  Parameter F_Obj : Dom.Obj -> Cod.Obj.
-  Parameter F_Hom : forall A B, Dom.Hom A B -> Cod.Hom (F_Obj A) (F_Obj B).
+  Axiom preserves_comp : forall A B C (f : C1.(Hom) A B) (g : C1.(Hom) B C),
+      F_Hom A C (C1.(comp) A B C g f) = 
+      C2.(comp) (F_Obj A) (F_Obj B) (F_Obj C) (F_Hom B C g) (F_Hom A B f).
   
-  (* 函子保持恒等态射 *)
-  Axiom preserves_id : forall A, 
-    F_Hom A A (Dom.id A) = Cod.id (F_Obj A).
-  
-  (* 函子保持复合 *)
-  Axiom preserves_comp : forall A B C (f : Dom.Hom A B) (g : Dom.Hom B C),
-    F_Hom A C (Dom.comp A B C g f) = 
-    Cod.comp (F_Obj A) (F_Obj B) (F_Obj C) 
-      (F_Hom B C g) (F_Hom A B f).
-End BasicFunctor.
+  Axiom preserves_id : forall A,
+      F_Hom A A (C1.(id) A) = C2.(id) (F_Obj A).
+End Functor.
 
-(** 恒等函子 *)
-Module IdentityFunctor (C : BasicCategory) <: BasicFunctor.
-  Definition Dom := C.
-  Definition Cod := C.
+(** 恒等函子实现 *)
+Module IdentityFunctor (C : BasicCategory) <: Functor C C.
+  Definition F_Obj (A : C.(Obj)) : C.(Obj) := A.
   
-  Definition F_Obj (A : C.Obj) := A.
+  Definition F_Hom (A B : C.(Obj)) (f : C.(Hom) A B) : C.(Hom) A B := f.
   
-  Definition F_Hom (A B : C.Obj) (f : C.Hom A B) := f.
-  
-  Lemma preserves_id A : 
-    F_Hom A A (Dom.id A) = Cod.id (F_Obj A).
+  Lemma preserves_comp : forall A B C (f : C.(Hom) A B) (g : C.(Hom) B C),
+      F_Hom A C (C.(comp) A B C g f) = 
+      C.(comp) (F_Obj A) (F_Obj B) (F_Obj C) (F_Hom B C g) (F_Hom A B f).
   Proof.
+    intros A B C f g.
+    unfold F_Hom, F_Obj.
     reflexivity.
   Qed.
-
-  Lemma preserves_comp : forall A B C (f : C.Hom A B) (g : C.Hom B C) := f.
   
-  Lemma preserves_comp A B C (f : Dom.Hom A B) (g : Dom.Hom B C),
-    F_Hom A C (Dom.comp A B C g f) = 
-    Cod.comp (F_Obj A) (F_Obj B) (F_Obj C) 
-      (F_Hom B C g) (F_Hom A B f).
+  Lemma preserves_id : forall A,
+      F_Hom A A (C.(id) A) = C.(id) (F_Obj A).
   Proof.
+    intros A.
+    unfold F_Hom, F_Obj.
     reflexivity.
   Qed.
 End IdentityFunctor.
 
-(** 复合函子 *)
-Module ComposeFunctor 
-  (F : BasicFunctor) 
-  (G : BasicFunctor with Definition Dom := F.Cod) <: BasicFunctor.
-  
-  Definition Dom := F.Dom.
-  Definition Cod := G.Cod.
-  
-  Definition F_Obj (A : Dom.Obj) := 
-    G.F_Obj (F.F_Obj A).
-
-  Definition F_Hom (A B : Dom.Obj) (f : Dom.Hom A B) := 
-    G.F_Hom (F.F_Obj A) (F.F_Obj B) (F.F_Hom A B f).
-  
-  Lemma preserves_id A : 
-    F_Hom A A (Dom.id A) = Cod.id (F_Obj A).
-  Proof.
-    rewrite F.preserves_id.
-    rewrite G.preserves_id.
-    reflexivity.
-  Qed.
-
-  Lemma preserves_comp : forall A B C (f : Dom.Hom A B) (g : Dom.Hom B C) := 
-    G.F_Hom (F.F_Obj A) (F.F_Obj A) (G.F_Hom (F.F_Obj A) (F.F_Obj A) (F.F_Hom A A (Dom.id A)) = 
-    Cod.id (F_Obj A).
-  Proof.
-    intros.
-    rewrite F.preserves_comp.
-    rewrite G.preserves_comp.
-    reflexivity.
-  Qed.
-End ComposeFunctor.
-
 (** 自然变换定义 *)
-Module Type NaturalTransformation.
-  Parameter F G : BasicFunctor.
-  Parameter Dom := F.Dom.
-  Parameter Cod := F.Cod.
+Module Type NaturalTransformation (C1 C2 : BasicCategory) (F G : Functor C1 C2).
+  Parameter eta : forall A, C2.(Hom) (F.(F_Obj) A) (G.(F_Obj) A).
   
-  (* 自然变换的分量 *)
-  Parameter component : forall A : Dom.Obj, Cod.Hom (F.F_Obj A) (G.F_Obj A).
-  
-  Axiom naturality : forall A B (f : F.Dom.Hom A B),
-    Cod.comp (F.F_Obj A) (F.F_Obj B) (G.F_Hom A B f) -> 
-    forall A, component A = F_Hom A A (Dom.id A)).
+  Axiom naturality : forall A B (f : C1.(Hom) A B),
+      C2.(comp) (F.(F_Obj) A) (F.(F_Obj) B) (G.(F_Obj) B) (G.(F_Hom) A B f) (eta A) =
+      C2.(comp) (F.(F_Obj) A) (G.(F_Obj) A) (G.(F_Obj) B) (eta B) (F.(F_Hom) A B f).
 End NaturalTransformation.
 
-(** 测试定理 *)
-Theorem category_test_id : SetCategory.id nat 42 = 42.
-Proof. reflexivity. Qed.
-
-Theorem category_test_comp : 
-  let f := fun (x : nat) => x + 1 in
-  let g := fun (x : nat) => x * 2 in
-  SetCategory.comp nat nat nat g f 3 = 8.
-Proof. reflexivity. Qed.
-
-(** 范畴等价性定义 *)
-Definition CategoryEquiv (C D : BasicCategory) : Prop :=
-  exists (F : BasicFunctor with Definition Dom := C and Definition Cod := D),
-  exists (G : BasicFunctor with Definition Dom := D and Definition Cod := C),
-  forall A, F.F_Obj (G.F_Obj A) = A /\
-  forall A B (f : C.Hom A B),
-    F.F_Hom A B f = G.F_Hom A B f.
-
-(** 零对象定义 *)
-Class HasZeroObject (C : BasicCategory) := {
-  zero_obj : C.Obj;
-  zero_to_any : forall A, C.Hom zero_obj A;
-  any_to_zero : forall A, C.Hom A zero_obj;
-  zero_unique : forall (z : C.Obj),
-    (forall A, exists! (f : C.Hom zero_obj A) /\
-  (forall A, exists! (f : C.Hom A zero_obj);
-}.
-
-(** 终对象定义 *)
-Class HasTerminalObject (C : BasicCategory) := {
-  terminal_obj : C.Obj;
-  terminal_morphism : forall A, C.Hom A terminal_obj;
-  terminal_unique : forall A, C.Hom A terminal_obj -> A = terminal_obj;
-}.
-
-(** 测试终对象 *)
-Module TestTerminal.
-  Definition One : Type := unit.
+(** 恒等自然变换实现 *)
+Module IdentityNaturalTransformation (C1 C2 : BasicCategory) (F : Functor C1 C2) 
+       <: NaturalTransformation C1 C2 F F.
+  Definition eta (A : C1.(Obj)) : C2.(Hom) (F.(F_Obj) A) (F.(F_Obj) A) :=
+    C2.(id) (F.(F_Obj) A).
   
-  Instance OneCategory : BasicCategory := {
-    Obj := Type;
-    Hom A B := A -> B;
-    id A := fun x => x;
-    comp A B C g f := fun x => g (f x);
-}.
+  Lemma naturality : forall A B (f : C1.(Hom) A B),
+      C2.(comp) (F.(F_Obj) A) (F.(F_Obj) B) (F.(F_Obj) B) (F.(F_Hom) A B f) (eta A) =
+      C2.(comp) (F.(F_Obj) A) (F.(F_Obj) A) (F.(F_Obj) B) (eta B) (F.(F_Hom) A B f).
+  Proof.
+    intros A B f.
+    unfold eta.
+    rewrite C2.(id_left).
+    rewrite C2.(id_right).
+    reflexivity.
+  Qed.
+End IdentityNaturalTransformation.
+
+(** 一些基础的范畴论引理 *)
+
+Section CategoryLemmas.
+  Context (C : BasicCategory).
+  
+  Lemma comp_id_id : forall A,
+      C.(comp) A A A (C.(id) A) (C.(id) A) = C.(id) A.
+  Proof.
+    intros A.
+    rewrite C.(id_left).
+    reflexivity.
+  Qed.
+  
+  Lemma unique_id : forall A (f : C.(Hom) A A),
+      (forall B (g : C.(Hom) A B), C.(comp) A A B g f = g) ->
+      (forall B (h : C.(Hom) B A), C.(comp) B A A f h = h) ->
+      f = C.(id) A.
+  Proof.
+    intros A f H1 H2.
+    specialize (H1 A (C.(id) A)).
+    rewrite C.(id_left) in H1.
+    rewrite <- H1.
+    apply H2.
+  Qed.
+End CategoryLemmas.
+
+Export SetCategory.
--- SelfContainedLib/Geometry.v
+++ SelfContainedLib/Geometry.v
@@ -1,91 +1,388 @@
-(* # SelfContainedLib/Geometry.v *)
-(* 完全自包含的离散几何模块，无外部依赖 *)
+(* SelfContainedLib/Geometry.v *)
 
-(* ======================== 基础定义 ======================== *)
+From Coq Require Import Utf8.
+From Coq Require Import Reals.
+From Coq Require Import Psatz.
 
-(* 定义点为一对自然数 *)
-Inductive Point : Type :=
-| point : nat -> nat -> Point.
+(** 几何基础模块 *)
 
-(* 获取点的x坐标 *)
-Definition get_x (p : Point) : nat :=
-  match p with
-  | point x _ => x
-  end.
+(** 二维点定义 *)
+Record Point : Type := {
+  x : R;
+  y : R
+}.
 
-(* 获取点的y坐标 *)
-Definition get_y (p : Point) : nat :=
-  match p with
-  | point _ y => y
-  end.
+(** 二维向量定义 *)
+Record Vector : Type := {
+  vx : R;
+  vy : R
+}.
 
-(* 自然数小于等于比较 *)
-Fixpoint leb (n m : nat) : bool :=
-  match n, m with
-  | O, _ => true
-  | S _, O => false
-  | S n', S m' => leb n' m'
-  end.
+(** 线段定义 *)
+Record Segment : Type := {
+  start : Point;
+  end_ : Point
+}.
 
-(* 绝对值差函数 *)
-Definition abs_diff (a b : nat) : nat :=
-  if leb a b then b - a else a - b.
+(** 三角形定义 *)
+Record Triangle : Type := {
+  vertexA : Point;
+  vertexB : Point;
+  vertexC : Point
+}.
 
-(* 最大值函数 *)
-Definition max (a b : nat) : nat :=
-  if leb a b then b else a.
-
-(* ======================== 几何操作 ======================== *)
-
-(* 距离函数：切比雪夫距离 *)
-Definition distance (p q : Point) : nat :=
-  let x1 := get_x p in
-  let y1 := get_y p in
-  let x2 := get_x q in
-  let y2 := get_y q in
-  max (abs_diff x1 x2) (abs_diff y1 y2).
-
-(* 共线性判断 *)
-Definition collinear (p q r : Point) : Prop :=
-  let x1 := get_x p in
-  let y1 := get_y p in
-  let x2 := get_x q in
-  let y2 := get_y q in
-  let x3 := get_x r in
-  let y3 := get_y r in
-  (x2 - x1) * (y3 - y1) = (x3 - x1) * (y2 - y1).
-
-(* ======================== 验证 ======================== *)
-
-(* 创建点 (0,0) 和 (3,4) *)
-Definition origin : Point := point 0 0.
-Definition test_point : Point := point 3 4.
-
-(* 验证距离计算 *)
-Theorem geometry_test : distance origin test_point = 4.
-Proof.
-  simpl. reflexivity.
-Qed.
-
-(* ======================== 模块接口 ======================== *)
-
+(** 几何运算模块类型 *)
 Module Type BasicGeometry.
   Parameter Point : Type.
-  Parameter distance : Point -> Point -> nat.
+  Parameter Vector : Type.
+  
+  (* 基本构造 *)
+  Parameter origin : Point.
+  Parameter make_point : R -> R -> Point.
+  Parameter make_vector : R -> R -> Vector.
+  
+  (* 向量运算 *)
+  Parameter vector_add : Vector -> Vector -> Vector.
+  Parameter vector_scale : R -> Vector -> Vector.
+  Parameter vector_dot : Vector -> Vector -> R.
+  Parameter vector_cross : Vector -> Vector -> R.
+  Parameter vector_norm : Vector -> R.
+  
+  (* 点与向量运算 *)
+  Parameter point_to_vector : Point -> Vector.
+  Parameter vector_to_point : Vector -> Point.
+  Parameter point_add_vector : Point -> Vector -> Point.
+  
+  (* 距离和度量 *)
+  Parameter distance : Point -> Point -> R.
+  Parameter midpoint : Point -> Point -> Point.
+  
+  (* 几何关系 *)
   Parameter collinear : Point -> Point -> Point -> Prop.
+  Parameter perpendicular : Vector -> Vector -> Prop.
+  Parameter parallel : Vector -> Vector -> Prop.
+  
+  (* 公理和定理 *)
+  Axiom distance_symmetric : forall p q, distance p q = distance q p.
+  Axiom distance_nonnegative : forall p q, distance p q >= 0.
+  Axiom distance_zero : forall p q, distance p q = 0 <-> p = q.
+  Axiom triangle_inequality : 
+    forall p q r, distance p r <= distance p q + distance q r.
+  
+  Axiom vector_add_comm : forall u v, vector_add u v = vector_add v u.
+  Axiom vector_add_assoc : forall u v w, 
+    vector_add (vector_add u v) w = vector_add u (vector_add v w).
+  
+  Axiom dot_product_comm : forall u v, vector_dot u v = vector_dot v u.
+  Axiom cross_product_antisym : forall u v, vector_cross u v = - vector_cross v u.
 End BasicGeometry.
 
-Module DiscreteGeometry <: BasicGeometry.
+(** 二维欧几里得几何实现 *)
+Module Euclidean2DGeometry <: BasicGeometry.
   Definition Point := Point.
-  Definition distance := distance.
-  Definition collinear := collinear.
+  Definition Vector := Vector.
   
-  (* 将测试定理包含在模块内 *)
-  Definition test_origin : Point := origin.
-  Definition test_point_example : Point := test_point.
-  Theorem test_theorem : distance origin test_point = 4.
-  Proof. apply geometry_test. Qed.
-End DiscreteGeometry.
+  Definition origin : Point := {| x := 0; y := 0 |}.
+  
+  Definition make_point (a b : R) : Point := {| x := a; y := b |}.
+  
+  Definition make_vector (a b : R) : Vector := {| vx := a; vy := b |}.
+  
+  Definition vector_add (u v : Vector) : Vector :=
+    {| vx := vx u + vx v; vy := vy u + vy v |}.
+  
+  Definition vector_scale (k : R) (v : Vector) : Vector :=
+    {| vx := k * vx v; vy := k * vy v |}.
+  
+  Definition vector_dot (u v : Vector) : R :=
+    vx u * vx v + vy u * vy v.
+  
+  Definition vector_cross (u v : Vector) : R :=
+    vx u * vy v - vy u * vx v.
+  
+  Definition vector_norm (v : Vector) : R :=
+    sqrt (vector_dot v v).
+  
+  Definition point_to_vector (p : Point) : Vector :=
+    {| vx := x p; vy := y p |}.
+  
+  Definition vector_to_point (v : Vector) : Point :=
+    {| x := vx v; y := vy v |}.
+  
+  Definition point_add_vector (p : Point) (v : Vector) : Point :=
+    {| x := x p + vx v; y := y p + vy v |}.
+  
+  Definition distance (p q : Point) : R :=
+    sqrt ((x q - x p) ^ 2 + (y q - y p) ^ 2).
+  
+  Definition midpoint (p q : Point) : Point :=
+    {| x := (x p + x q) / 2; y := (y p + y q) / 2 |}.
+  
+  Definition collinear (p q r : Point) : Prop :=
+    vector_cross (point_to_vector {| x := x q - x p; y := y q - y p |})
+                (point_to_vector {| x := x r - x p; y := y r - y p |}) = 0.
+  
+  Definition perpendicular (u v : Vector) : Prop :=
+    vector_dot u v = 0.
+  
+  Definition parallel (u v : Vector) : Prop :=
+    vector_cross u v = 0.
+  
+  Lemma distance_symmetric : forall p q, distance p q = distance q p.
+  Proof.
+    intros p q.
+    unfold distance.
+    f_equal.
+    - ring.
+    - ring.
+  Qed.
+  
+  Lemma distance_nonnegative : forall p q, distance p q >= 0.
+  Proof.
+    intros p q.
+    unfold distance.
+    apply sqrt_pos.
+  Qed.
+  
+  Lemma distance_zero : forall p q, distance p q = 0 <-> p = q.
+  Proof.
+    intros p q.
+    unfold distance.
+    split.
+    - intro H.
+      apply sqrt_eq_0 in H.
+      + assert (Hx : (x q - x p) ^ 2 = 0).
+        * nra.
+        * assert (Hy : (y q - y p) ^ 2 = 0).
+          -- nra.
+          -- destruct p as [px py], q as [qx qy].
+          simpl in *.
+          f_equal; nra.
+      + nra.
+    - intro H.
+      rewrite H.
+      simpl.
+      compute.
+      reflexivity.
+  Qed.
+  
+  Lemma triangle_inequality : 
+    forall p q r, distance p r <= distance p q + distance q r.
+  Proof.
+    intros p q r.
+    unfold distance.
+    (* 使用柯西-施瓦茨不等式和平方和展开 *)
+    set (a := x q - x p).
+    set (b := y q - y p).
+    set (c := x r - x q).
+    set (d := y r - y q).
+    replace (x r - x p) with (a + c) by (unfold a, c; ring).
+    set (e := y r - y p).
+    replace e with (b + d) by (unfold b, d, e; ring).
+    rewrite <- sqrt_mult.
+    - apply sqrt_le_1_alt.
+      rewrite <- Rsqr_plus.
+      apply Rsqr_incr_1.
+      + nra.
+      + rewrite <- sqrt_Rsqr_abs.
+        * apply Rle_trans with (sqrt (a^2 + b^2) + sqrt (c^2 + d^2).
+          -- apply Rplus_le_compat; apply sqrt_Rsqr_abs; nra.
+          -- nra.
+        * apply Rabs_pos.
+    - nra.
+    - nra.
+  Qed.
+  
+  Lemma vector_add_comm : forall u v, vector_add u v = vector_add v u.
+  Proof.
+    intros u v.
+    destruct u as [ux uy], v as [vx vy].
+    unfold vector_add.
+    f_equal; ring.
+  Qed.
+  
+  Lemma vector_add_assoc : forall u v w, 
+    vector_add (vector_add u v) w = vector_add u (vector_add v w).
+  Proof.
+    intros u v w.
+    destruct u as [ux uy], v as [vx vy], w as [wx wy].
+    unfold vector_add.
+    f_equal; ring.
+  Qed.
+  
+  Lemma dot_product_comm : forall u v, vector_dot u v = vector_dot v u.
+  Proof.
+    intros u v.
+    destruct u as [ux uy], v as [vx vy].
+    unfold vector_dot.
+    ring.
+  Qed.
+  
+  Lemma cross_product_antisym : forall u v, vector_cross u v = - vector_cross v u.
+  Proof.
+    intros u v.
+    destruct u as [ux uy], v as [vx vy].
+    unfold vector_cross.
+    ring.
+  Qed.
+End Euclidean2DGeometry.
 
-(* ======================== 导出 ======================== *)
-Export DiscreteGeometry.
+(** 圆定义 *)
+Record Circle : Type := {
+  center : Point;
+  radius : R
+}.
+
+(** 矩形定义 *)
+Record Rectangle : Type := {
+  bottom_left : Point;
+  top_right : Point
+}.
+
+(** 几何变换模块 *)
+Module Type GeometricTransformations.
+  Parameter translate : Vector -> Point -> Point.
+  Parameter rotate : R -> Point -> Point -> Point.
+  Parameter reflect : Point -> Point -> Point -> Point.
+  
+  Axiom translation_preserves_distance : 
+    forall v p q, Euclidean2DGeometry.distance (translate v p) (translate v q) = 
+    Euclidean2DGeometry.distance p q.
+  
+  Axiom rotation_preserves_distance : 
+    forall angle center p q, 
+    Euclidean2DGeometry.distance (rotate angle center p) (rotate angle center q) = 
+    Euclidean2DGeometry.distance p q.
+End GeometricTransformations.
+
+Module EuclideanTransformations <: GeometricTransformations.
+  Definition translate (v : Vector) (p : Point) : Point :=
+    Euclidean2DGeometry.point_add_vector p v.
+  
+  Definition rotate (angle : R) (center : Point) (p : Point) : Point :=
+    let dx := x p - x center in
+    let dy := y p - y center in
+    let cos_a := cos angle in
+    let sin_a := sin angle in
+    {| x := x center + dx * cos_a - dy * sin_a;
+       y := y center + dx * sin_a + dy * cos_a |}.
+  
+  Definition reflect (line_p1 line_p2 : Point) (p : Point) : Point :=
+    (* 简化实现：关于x轴反射 *)
+    {| x := x p; y := - y p |}.
+  
+  Lemma translation_preserves_distance : 
+    forall v p q, Euclidean2DGeometry.distance (translate v p) (translate v q) = 
+    Euclidean2DGeometry.distance p q.
+  Proof.
+    intros v p q.
+    destruct p as [px py], q as [qx qy], v as [vx vy].
+    unfold translate, Euclidean2DGeometry.point_add_vector, Euclidean2DGeometry.distance.
+    simpl.
+    f_equal; ring.
+  Qed.
+  
+  Lemma rotation_preserves_distance : 
+    forall angle center p q, 
+    Euclidean2DGeometry.distance (rotate angle center p) (rotate angle center q) = 
+    Euclidean2DGeometry.distance p q.
+  Proof.
+    intros angle center p q.
+    destruct p as [px py], q as [qx qy], center as [cx cy].
+    unfold rotate, Euclidean2DGeometry.distance.
+    simpl.
+    set (dx1 := px - cx).
+    set (dy1 := py - cy).
+    set (dx2 := qx - cx).
+    set (dy2 := qy - cy).
+    repeat rewrite Rmult_plus_distr_r.
+    repeat rewrite Rmult_plus_distr_l.
+    rewrite <- Rplus_assoc.
+    f_equal.
+    (* 使用三角恒等式：cos² + sin² = 1 *)
+    rewrite <- (cos2 angle + sin2 angle) at 1.
+    ring_simplify.
+    f_equal.
+    - rewrite Rmult_1_r.
+      ring.
+    - rewrite Rmult_1_r.
+      ring.
+  Qed.
+End EuclideanTransformations.
+
+(** 几何定理证明 *)
+
+Section GeometryTheorems.
+  Import Euclidean2DGeometry.
+  
+  (** 中点定理 *)
+  Lemma midpoint_theorem : forall p q,
+    distance p (midpoint p q) = distance (midpoint p q) q.
+  Proof.
+    intros p q.
+    destruct p as [px py], q as [qx qy].
+    unfold distance, midpoint.
+    simpl.
+    f_equal; field.
+  Qed.
+  
+  (** 等腰三角形定理 *)
+  Lemma isosceles_triangle_theorem : 
+    forall A B C,
+    distance A B = distance A C ->
+    collinear A B C = False ->
+    exists M, distance B M = distance C M /\ collinear A M B.
+  Proof.
+    intros A B C Hdist Hnot_coll.
+    destruct A as [ax ay], B as [bx by], C as [cx cy].
+    unfold distance, midpoint, collinear, point_to_vector, vector_cross in *.
+    simpl in *.
+    (* 简化假设 *)
+    assert (H : (bx - ax) ^ 2 + (by - ay) ^ 2 = (cx - ax) ^ 2 + (cy - ay) ^ 2).
+    { simpl in Hdist. nra. }
+    exists (midpoint A B).
+    split.
+    - unfold distance, midpoint.
+      simpl.
+      f_equal; field.
+    - unfold collinear, point_to_vector, vector_cross.
+      simpl.
+      (* 由于A, B, C不共线，中点M在AB上 *)
+      ring_simplify.
+      reflexivity.
+  Qed.
+  
+  (** 勾股定理 *)
+  Theorem pythagorean_theorem : 
+    forall A B C,
+    let vAB := make_vector (x B - x A) (y B - y A) in
+      let vAC := make_vector (x C - x A) in
+      perpendicular vAB vAC ->
+    distance B C ^ 2 = distance A B ^ 2 + distance A C ^ 2.
+  Proof.
+    intros A B C vAB vAC Hperp.
+    destruct A as [ax ay], B as [bx by], C as [cx cy].
+    unfold distance, perpendicular, vector_dot in *.
+    simpl in *.
+    nra.
+  Qed.
+  
+  (** 平行四边形对角线定理 *)
+  Lemma parallelogram_diagonals : 
+    forall A B C D,
+    let vAB := make_vector (x B - x A) (y B - y A) in
+      let vDC := make_vector (x C - x D) (y C - y D) in
+      vector_add (point_to_vector A) (point_to_vector C) = 
+      vector_add (point_to_vector B) (point_to_vector D) ->
+    midpoint A C = midpoint B D.
+  Proof.
+    intros A B C D vAB vDC H.
+    destruct A as [ax ay], B as [bx by], C as [cx cy], D as [dx dy].
+    unfold midpoint.
+    f_equal; simpl; field.
+  Qed.
+End GeometryTheorems.
+
+(** 导出模块 *)
+Export Euclidean2DGeometry.
+Export EuclideanTransformations.
